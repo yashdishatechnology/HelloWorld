7283a452f2f9b01274caabfae1025d8ce1cab903
Yash Rathod <lifeiscrazy2332@gmail.com>
2017-09-07 15:53:58 +0530

    ädded .gitignore .gitattributes

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btn:UIButton!
+    @IBOutlet weak var lbl1:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,12 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func btnClick(_ sender:UIButton) {
+        
+        lbl1.text = "Hello World"
+        
     }
 
 
